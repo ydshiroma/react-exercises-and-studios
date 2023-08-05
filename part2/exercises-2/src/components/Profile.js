94ad465f25cb47e7cb1fe4a85e9b0f8d42823046
Yumi <ydshiroma@gmail.com>
2023-08-05 19:45:11 -0400

    all but button functionality

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,14 +1,25 @@
 import './styles.css';
 import Button from './Button';
-
+import oceans from './oceans.json';
 
 function Profile()
 {
+   const listItem = oceans.map(ocean => 
+      <div key={ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+         <img src={ocean.image} alt={ocean.name}/>
+         <h1>{ocean.name}</h1>
+         <h3>Facts</h3>
+         <ol>
+            <li>{ocean.fact1}</li>
+            <li>{ocean.fact2}</li>
+            <li>{ocean.fact3}</li>
+            <Button />
+         </ol>
+      </div>);
    return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+         <ul>
+            {listItem}
+         </ul>
    );
 }
 
